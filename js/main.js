ca8c6bc9af5018a239aa386b349c4dfcd8bc050f
Anne <ajoseph13@ggc.edu>
2020-09-13 13:41:14 -0400

    added indicator functionality to the tab header

--- js/main.js
+++ js/main.js
@@ -26,13 +26,18 @@ function showContent(tabIndex){
 }
 
 let tabPanes = showContent("tab-header")[0].getElementsByTagName("div");
+let mq = window.matchMedia("screen and (min-width: 700px)");
 
 for(let i=0;i<tabPanes.length;i++){
     tabPanes[i].addEventListener("click",function(){
         showContent("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
         tabPanes[i].classList.add("active");
 
-        showContent("tab-indicator")[0].style.top = `calc(80px + ${i*50}px)`;
+        if (mq.matches) {
+            showContent("tab-indicator")[0].style.top = `calc(50px + ${i*50}px)`;
+        } else {
+            showContent("tab-indicator")[0].style.left = `calc(calc(100% / 3) * ${i})`;
+        }
 
         showContent("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
         showContent("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
